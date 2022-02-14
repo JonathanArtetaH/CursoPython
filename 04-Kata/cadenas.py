60ff414c4d4a0944125a916495e78fb9e395f711
Jonathan AH <54410480+JonathanArtetaH@users.noreply.github.com>
2022-02-14 09:34:47 -0600

    Se suben archivos de las demas katas

---
+++ 04-Kata/cadenas.py
@@ -0,0 +1,10 @@
+text = """Interesting facts about the Moon. The Moon is Earth's only satellite. There are several interesting facts about the Moon and how it affects life here on Earth. 
+On average, the Moon moves 4cm away from the Earth every year. This yearly drift is not significant enough to cause immediate effects on Earth. The highest daylight temperature of the Moon is 127 C."""
+  
+
+palabraBuscar='C';
+
+oraciones = text.split()
+for palabra in oraciones:
+    if(palabra == palabraBuscar):
+        print('Se contro con :',palabra)
---
+++ 3-kata/condicionales.py
@@ -0,0 +1,33 @@
+velocidadAsteroide = 49 
+
+#-----------------------------------------
+print(" --- Problema Numero Uno ---")
+if velocidadAsteroide > 25:
+    print('Ay no, ya valimos')
+else:
+    print('No hay peligro en este momento')
+
+#----------------------------------------
+print("\n --- Problema Numero Dos ---")
+velocidadAsteroide = 20
+if velocidadAsteroide > 20:
+    print('Hay una luz mágica en el cielo')
+elif velocidadAsteroide == 20:
+    print('Hay una luz mágica en el cielo')
+else:
+    print('Nada que ver')
+
+#-----------------------------------------
+print("\n --- Problema Numero Tres ---")
+
+velocidadAsteroide = 25
+tamAsteroide = 40 
+
+if velocidadAsteroide > 25 and tamAsteroide > 25:
+    print('Hoy es el ultimo dia para vivir... :(')
+elif velocidadAsteroide >= 20:
+    print('Hay una luz magica en el cielo !')
+elif tamAsteroide < 25:
+    print('Nada que ver')
+else:
+    print('Nada que ver')
